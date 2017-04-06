d0029ef64da62648e751e77e7683b200396fca6a
Juan Ageitos <andresageitos@gmail.com>
2017-04-06 01:05:26 -0300

    Agregar archivos de proyecto.

---
+++ KBManagerUI/Menu.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace KBManagerUI
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnConfiguration = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(16, 13);
+            this.btnRestore.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(291, 82);
+            this.btnRestore.TabIndex = 0;
+            this.btnRestore.Text = "Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(16, 105);
+            this.btnBackup.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(291, 82);
+            this.btnBackup.TabIndex = 1;
+            this.btnBackup.Text = "Backup";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            // 
+            // btnConfiguration
+            // 
+            this.btnConfiguration.Location = new System.Drawing.Point(16, 196);
+            this.btnConfiguration.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnConfiguration.Name = "btnConfiguration";
+            this.btnConfiguration.Size = new System.Drawing.Size(291, 82);
+            this.btnConfiguration.TabIndex = 2;
+            this.btnConfiguration.Text = "Configuration";
+            this.btnConfiguration.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 295);
+            this.Controls.Add(this.btnConfiguration);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnRestore);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KB Manager Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnConfiguration;
+    }
+}
